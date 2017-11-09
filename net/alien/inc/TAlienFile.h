ca509d64bfd8205c6dfb912bf85995489315643c
Jan Knedlik <j.knedlik@gsi.de>
2017-11-09 14:17:08 +0100

    TAlienFile is derived from TXNGNetFile

--- net/alien/inc/TAlienFile.h
+++ net/alien/inc/TAlienFile.h
@@ -33,10 +33,9 @@
 
 #include "TXNetFile.h"
 
-
 class TUrl;
 
-class TAlienFile : public TXNetFile {
+class TAlienFile : public TXNGNetFile {
 
 private:
    TString fLfn;       // logical file name
@@ -45,46 +44,47 @@ private:
    TString fUrl;       // url for the opened copy
    TString fPfn;       // physical file name
    TString fSE;        // Storage element
-   Int_t   fImage;     // Image number
-   Int_t   fNreplicas; // Number of replicas
+   Int_t fImage;       // Image number
+   Int_t fNreplicas;   // Number of replicas
    Long64_t fOpenedAt; // Absolute value for time when opened
    Double_t fElapsed;  // Time elapsed to opem file
 public:
-   TAlienFile() : TXNetFile(), fLfn(), fAuthz(), fGUID(), fUrl(), fPfn(), fSE(), fImage(0), fNreplicas(0), fOpenedAt(0), fElapsed(0) { }
-   TAlienFile(const char *purl, Option_t *option = "",
-              const char *ftitle = "", Int_t compress = 1,
-              Bool_t parallelopen = kFALSE, const char *lurl = 0,
-              const char *authz = 0);
+   TAlienFile()
+      : TXNetFile(), fLfn(), fAuthz(), fGUID(), fUrl(), fPfn(), fSE(), fImage(0), fNreplicas(0), fOpenedAt(0),
+        fElapsed(0)
+   {
+   }
+   TAlienFile(const char *purl, Option_t *option = "", const char *ftitle = "", Int_t compress = 1,
+              Bool_t parallelopen = kFALSE, const char *lurl = 0, const char *authz = 0);
    virtual ~TAlienFile();
 
    virtual void Close(const Option_t *opt = "");
 
-   const char  *GetGUID() const  {return fGUID;}
-   Double_t     GetElapsed() const {return fElapsed;}
-   Int_t        GetImage() const {return fImage;}
-   const char  *GetLfn() const   {return fLfn;}
-   Int_t        GetNreplicas() const {return fNreplicas;}
-   Long64_t     GetOpenTime() const {return fOpenedAt;}
-   const char  *GetPfn() const   {return fPfn;}
-   const char  *GetSE() const    {return fSE;}
-   const char  *GetUrl() const   {return fUrl;}
+   const char *GetGUID() const { return fGUID; }
+   Double_t GetElapsed() const { return fElapsed; }
+   Int_t GetImage() const { return fImage; }
+   const char *GetLfn() const { return fLfn; }
+   Int_t GetNreplicas() const { return fNreplicas; }
+   Long64_t GetOpenTime() const { return fOpenedAt; }
+   const char *GetPfn() const { return fPfn; }
+   const char *GetSE() const { return fSE; }
+   const char *GetUrl() const { return fUrl; }
 
 protected:
-   void         SetGUID(const char *guid) {fGUID = guid;}
-   void         SetElapsed(Double_t real) {fElapsed = real;}
-   void         SetImage(Int_t image)   {fImage = image;}
-   void         SetNreplicas(Int_t nrep) {fNreplicas = nrep;}
-   void         SetPfn(const char *pfn) {fPfn = pfn;}
-   void         SetSE(const char *se)   {fSE = se;}
-   void         SetUrl(const char *url) {fUrl = url;}
+   void SetGUID(const char *guid) { fGUID = guid; }
+   void SetElapsed(Double_t real) { fElapsed = real; }
+   void SetImage(Int_t image) { fImage = image; }
+   void SetNreplicas(Int_t nrep) { fNreplicas = nrep; }
+   void SetPfn(const char *pfn) { fPfn = pfn; }
+   void SetSE(const char *se) { fSE = se; }
+   void SetUrl(const char *url) { fUrl = url; }
 
 public:
-   static TAlienFile *Open(const char *lfn, const Option_t *option = "",
-                           const char *title = "", Int_t compress = 1,
+   static TAlienFile *Open(const char *lfn, const Option_t *option = "", const char *title = "", Int_t compress = 1,
                            Bool_t parallelopen = kFALSE);
-   static TString     SUrl(const char *lfn);
+   static TString SUrl(const char *lfn);
 
-   ClassDef(TAlienFile, 4)  //A ROOT file that reads/writes via AliEn services and TXNetFile protocol
+   ClassDef(TAlienFile, 4) // A ROOT file that reads/writes via AliEn services and TXNetFile protocol
 };
 
 #endif
